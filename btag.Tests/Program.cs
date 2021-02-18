e6f8cd33273c58ed23dbf9e99f5fc38b2856c86b
komunre <koliziy5@gmail.com>
2021-02-18 19:45:48 +0700

    More tests, new parser method

--- btag.Tests/Program.cs
+++ btag.Tests/Program.cs
@@ -20,6 +20,7 @@ namespace btag.Tests
             var subSecond2 = new Tag("subsecond2");
             var deeper = new Tag("deeper");
             second.value = new byte[]{ 0x62, 0x69, 0x67, 0x20, 0x74, 0x65, 0x73, 0x74, 0x20, 0x28, 0x62, 0x69, 0x67, 0x29 };
+            subSecond2.value = new byte[] { 0x02 };
             main.AddChild(first);
             main.AddChild(second);
             second.AddChild(subSecond);
@@ -39,6 +40,12 @@ namespace btag.Tests
             var mainLayer = parser.FindTagLayerRoot("main");
             var secondLayer = parser.FindTagLayer(mainLayer, "second");
             Console.WriteLine(Encoding.Default.GetString(secondLayer.value));
+
+            secondLayer.value = Encoding.Default.GetBytes("hello world");
+
+            writer.OpenStream("output3.btag");
+            writer.WriteAll(mainLayer);
+            writer.CloseStream();
         }
     }
 }
