8cf9f97c8105cbc56bcdcaebbe8689247487f310
komunre <koliziy5@gmail.com>
2021-02-19 15:27:10 +0700

    Server sockets added (No working server yet)

---
+++ btag.Server/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace btag.Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // TODO: Server parsing, receiving, sending, writing
+        }
+    }
+}
---
+++ btag.Tests/Server.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net.Sockets;
+using System.Net;
+using System.Threading;
+
+namespace btag.Tests
+{
+    public class Server
+    {
+        public string data = null;
+
+        public void Listen(int port)
+        {
+            IPHostEntry entry = Dns.GetHostEntry(Dns.GetHostName());
+            IPAddress ipAddress = entry.AddressList[0];
+            IPEndPoint endPoint = new IPEndPoint(ipAddress, port);
+
+            Socket listener = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            listener.Bind(endPoint);
+            listener.Listen(10);
+
+            Socket[] clients = new Socket[100];
+            while (true)
+            {
+                Thread.Sleep(50);
+                clients[GetNullClient(clients)] = listener.Accept();
+                data = null;
+
+
+            }
+        }
+
+        public int GetNullClient(in Socket[] clients)
+        {
+            for (int x = 0; x < clients.Length; x++)
+            {
+                if (clients[x] == null)
+                {
+                    return x;
+                }
+            }
+            return 0;
+        }
+    }
+}
